cfdb86527b538bc16bbcf83b9067cccf63ae7eb9
Wilfred Kigenyi <wkigenyi@gmail.com>
2023-09-10 19:52:50 +0300

    Change the Head Title

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -27,7 +27,7 @@ const App = (props) => {
     <CacheProvider value={emotionCache}>
       <Head>
         <title>
-          Devias Kit
+          ABC Recon App
         </title>
         <meta
           name="viewport"
